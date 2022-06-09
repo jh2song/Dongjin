f8a61ad7616aec3ee9ccfe61add447fcd4eb9b28
Jihoon Song <spec0003@naver.com>
2022-06-09 10:43:42 +0900

    Commit 22/06/09 오전 10:43

--- Dongjin/MainWindow.xaml.cs
+++ Dongjin/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Dongjin
 {
@@ -23,7 +25,37 @@ namespace Dongjin
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			/* 참고
+			/* https://post.naver.com/viewer/postView.nhn?volumeNo=6903303&memberNo=11439725
+			*/
+			DispatcherTimer timer = new DispatcherTimer();
+			timer.Interval = new TimeSpan(0, 0, 1);
+			timer.Tick += Timer_Tick;
+			timer.Start();
 		}
 
+		private void Timer_Tick(object sender, EventArgs e)
+		{
+			var fd = new FlowDocument();
+			
+			fd.TextAlignment = TextAlignment.Right;
+
+			Paragraph paragraph = new Paragraph();
+
+			paragraph.Inlines.Add(new Run("오늘은 ") { Foreground = Brushes.White });
+			paragraph.Inlines.Add(new Run(DateTime.Now.Year.ToString().Substring(2, 2)) { Foreground = Brushes.Red });
+			paragraph.Inlines.Add(new Run("년 ") { Foreground = Brushes.White });
+			paragraph.Inlines.Add(new Run(DateTime.Now.Month.ToString("00")) { Foreground = Brushes.Red });
+			paragraph.Inlines.Add(new Run("월 ") { Foreground = Brushes.White });
+			paragraph.Inlines.Add(new Run(DateTime.Now.Day.ToString("00")) { Foreground = Brushes.Red });
+			paragraph.Inlines.Add(new Run("일 ") { Foreground = Brushes.White });
+			paragraph.Inlines.Add(new Run(DateTime.Now.ToString("ddd")) { Foreground = Brushes.Red });
+			paragraph.Inlines.Add(new Run("요일") { Foreground = Brushes.White });
+
+			fd.Blocks.Add(paragraph);
+
+			rtbDate.Document = fd;
+		}
 	}
 }
