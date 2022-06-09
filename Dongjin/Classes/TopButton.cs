10070ee4f84d55231270006a7b9e3f2d6b230369
지훈 송 <spec0003@naver.com>
2022-06-09 15:15:31 +0900

    Commit 22/06/09 오후 3:15

---
+++ Dongjin/Classes/TopButton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Dongjin.Classes
+{
+	class TopButton
+	{
+		static List<Button> DownButton { get; set; }
+	}
+}
--- Dongjin/MainWindow.xaml.cs
+++ Dongjin/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dongjin.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,10 +23,23 @@ namespace Dongjin
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private List<Button> topButtons = new List<Button>();
+		private List<List<Button>> underButtons = new List<List<Button>>();
+		private List<Button> underButtons1 = new List<Button>();
+		private List<Button> underButtons2 = new List<Button>();
+		private List<Button> underButtons3 = new List<Button>();
+		private List<Button> underButtons4 = new List<Button>();
+		private List<Button> underButtons5 = new List<Button>();
+		private List<Button> underButtons6 = new List<Button>();
+		private List<Border> borders = new List<Border>();
+
+		private bool underVisible = false;
+
 		public MainWindow()
 		{
 			InitializeComponent();
 
+			/* 시계
 			/* 참고
 			/* https://post.naver.com/viewer/postView.nhn?volumeNo=6903303&memberNo=11439725
 			*/
@@ -33,6 +47,44 @@ namespace Dongjin
 			timer.Interval = new TimeSpan(0, 0, 1);
 			timer.Tick += Timer_Tick;
 			timer.Start();
+
+			topButtons.Add(topButton1);
+			topButtons.Add(topButton2);
+			topButtons.Add(topButton3);
+			topButtons.Add(topButton4);
+			topButtons.Add(topButton5);
+			topButtons.Add(topButton6);
+
+			underButtons1.Add(underButton11);
+			underButtons1.Add(underButton12);
+			underButtons1.Add(underButton13);
+			underButtons1.Add(underButton14);
+
+			underButtons2.Add(underButton21);
+			underButtons2.Add(underButton22);
+
+			underButtons3.Add(underButton31);
+			underButtons3.Add(underButton32);
+			underButtons3.Add(underButton33);
+
+			underButtons6.Add(underButton61);
+			underButtons6.Add(underButton62);
+
+			underButtons.Add(underButtons1);
+			underButtons.Add(underButtons2);
+			underButtons.Add(underButtons3);
+			underButtons.Add(underButtons4);
+			underButtons.Add(underButtons5);
+			underButtons.Add(underButtons6);
+
+			borders.Add(borderUnder1);
+			borders.Add(borderUnder2);
+			borders.Add(borderUnder3);
+			borders.Add(new Border());
+			borders.Add(new Border());
+			borders.Add(borderUnder6);
+
+			topButtons[0].Focus();
 		}
 
 		private void Timer_Tick(object sender, EventArgs e)
@@ -56,6 +108,39 @@ namespace Dongjin
 			fd.Blocks.Add(paragraph);
 
 			rtbDate.Document = fd;
+		}
+
+		int topIndex = 0;
+		int underIndex = 0;
+		private void Window_KeyDown(object sender, KeyEventArgs e)
+		{
+			switch(e.Key)
+			{
+				case (Key.Enter):
+					underVisible = !underVisible;
+					break;
+				case (Key.Left):
+					topIndex = (topIndex - 1 + 6) % 6;
+					break;
+				case (Key.Right):
+					topIndex = (topIndex + 1) % 6;
+					break;
+
+				case (Key.Down):
+
+					break;
+				case (Key.Up):
+
+					break;
+			}
+
+			topButtons[topIndex].Focus();
+			borders[topIndex].BorderThickness = new Thickness(2, 2, 2, 2);
+			if (underVisible)
+			{
+				foreach (Button b in underButtons[topIndex])
+					b.Visibility = Visibility.Visible;
+			}
 		}
 	}
 }
