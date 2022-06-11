eb4b695d618e0383276f7e6ff523b62d294570c4
Jihoon Song <spec0003@naver.com>
2022-06-12 07:48:49 +0900

    에러 픽스

--- Dongjin/Windows/MenuWindow/MenuWindow.xaml.cs
+++ Dongjin/Windows/MenuWindow/MenuWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
-namespace Dongjin
+namespace Dongjin.Windows.MenuWindow
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
