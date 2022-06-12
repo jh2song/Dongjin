9f9f3afecf2f92d39e1dfadd716474c82236db7b
Jihoon Song <spec0003@naver.com>
2022-06-12 11:27:09 +0900

    하드코딩 덩어리들...

--- Dongjin/Windows/LoginWindow/LoginWindow.xaml.cs
+++ Dongjin/Windows/LoginWindow/LoginWindow.xaml.cs
@@ -17,9 +17,63 @@ namespace Dongjin.Windows.LoginWindow
 	/// </summary>
 	public partial class LoginWindow : Window
 	{
+		private List<Label> labels = new List<Label>();
+		private int index = -1;
+		private static string pw = "";
+
 		public LoginWindow()
 		{
 			InitializeComponent();
+
+			labels.Add(label1);
+			labels.Add(label2);
+			labels.Add(label3);
+			labels.Add(label4);
+		}
+		
+		private void Window_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Enter)
+			{
+				if (index == 1)
+				{
+
+				}
+				else if (index == 2)
+				{
+
+				}
+				else if (index == 3)
+					Close();
+			}
+			else if (e.Key == Key.Tab)
+			{
+				index++;
+				if (index > 3)
+					index = 0;
+			}
+			else if (index == 0)
+			{
+				pw += e.Key.ToString();
+				label1.Content += "_ ";
+			}
+
+			// 그리기 갱신
+			if (index >= 0 && index <= 3)
+			{
+				labels[index].Foreground = Brushes.Black;
+				labels[index].Background = Brushes.White;
+			}
+		}
+
+		// 그리기 초기화
+		private void Window_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (index >= 0 && index <= 3)
+			{
+				labels[index].Foreground = Brushes.White;
+				labels[index].Background = Brushes.Black;
+			}
 		}
 	}
 }
