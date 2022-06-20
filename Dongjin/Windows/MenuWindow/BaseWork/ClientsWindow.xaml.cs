08cd206da7b20773c8c97403edb95fc9e264acc1
Jihoon Song <spec0003@naver.com>
2022-06-21 08:05:39 +0900

    feat: 거래처코드에서 간단한 UX 개선

--- Dongjin/Windows/MenuWindow/BaseWork/ClientsWindow.xaml.cs
+++ Dongjin/Windows/MenuWindow/BaseWork/ClientsWindow.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Dongjin.Classes;
+
 
 namespace Dongjin.Windows.MenuWindow.BaseWork
 {
@@ -17,113 +19,50 @@ namespace Dongjin.Windows.MenuWindow.BaseWork
 	/// </summary>
 	public partial class ClientsWindow : Window
 	{
-		private readonly List<TextBox> TopTBList = new List<TextBox>();
-		private readonly List<TextBox> MiddleTBList = new List<TextBox>();
-		private int focusIdx;
 
 		public ClientsWindow()
 		{
 			InitializeComponent();
 
-			SetList();
 
 			tb1.Text = DateTime.Now.Year.ToString().Substring(2, 2);
 			tb2.Text = DateTime.Now.Month.ToString("00");
 			tb3.Text = DateTime.Now.Day.ToString("00");
 
-			focusIdx = 3;
-			TopTBList[focusIdx].Focus();
+			// 거래처코드에 포커싱
+			tb4.Focus();
 		}
 
 		private void SetList()
 		{
-			TopTBList.Add(tb1);
-			TopTBList.Add(tb2);
-			TopTBList.Add(tb3);
-			TopTBList.Add(tb4);
-
-			MiddleTBList.Add(tbDetail1);
-			MiddleTBList.Add(tbDetail2);
-			MiddleTBList.Add(tbDetail3);
-			MiddleTBList.Add(tbDetail4);
-		}
-
-		private void Window_KeyDown(object sender, KeyEventArgs e)
-		{
-			if (e.Key == Key.Escape)
-			{
-				Close();
-			}
-			else if (e.Key == Key.Enter)
-			{
-				if (TopTBList[focusIdx] == tb1)
-				{
-					TopTBList[focusIdx].Text = DateTime.Today.Year.ToString().Substring(2, 2);
-					focusIdx++;
-					TopTBList[focusIdx].Focus();
-				}
-				else if (TopTBList[focusIdx] == tb2)
-				{
-					TopTBList[focusIdx].Text = DateTime.Now.Month.ToString("00");
-					focusIdx++;
-					TopTBList[focusIdx].Focus();
-				}
-				else if (TopTBList[focusIdx] == tb3)
-				{
-					TopTBList[focusIdx].Text = DateTime.Now.Day.ToString("00");
-					focusIdx++;
-					TopTBList[focusIdx].Focus();
-				}
-			}
-		}
-
-		private void TB1_KeyUp(object sender, KeyEventArgs e)
-		{
-			if (tb1.Text.Length >= 2)
-			{
-				focusIdx++;
-				TopTBList[focusIdx].Focus();
-			}
-		}
-
-		private void TB2_KeyUp(object sender, KeyEventArgs e)
-		{
-			if (tb2.Text.Length >= 2)
-			{
-				focusIdx++;
-				TopTBList[focusIdx].Focus();
-			}
-			if (tb2.Text.Length == 0 && e.Key == Key.Back)
-			{
-				focusIdx--;
-				TopTBList[focusIdx].Focus();
-				TopTBList[focusIdx].Select(tb1.Text.Length, 0);
-			}
-		}
-
-		private void TB3_KeyUp(object sender, KeyEventArgs e)
-		{
-			if (tb3.Text.Length >= 2)
-			{
-				focusIdx++;
-				TopTBList[focusIdx].Focus();
-			}
-			if (tb3.Text.Length == 0 && e.Key == Key.Back)
-			{
-				focusIdx--;
-				TopTBList[focusIdx].Focus();
-				TopTBList[focusIdx].Select(tb2.Text.Length, 0);
-			}
 		}
 
 		private void TB4_KeyUp(object sender, KeyEventArgs e)
 		{
-			if (tb4.Text.Length == 0 && e.Key == Key.Back)
+			if (e.Key == Key.Escape)
 			{
-				focusIdx--;
-				TopTBList[focusIdx].Focus();
-				TopTBList[focusIdx].Select(tb3.Text.Length, 0);
+				if (tb4.Text == "")
+					Close();
+				else
+					tb4.Text = "";
 			}
+
+			if (e.Key == Key.Enter)
+			{
+				if (IsThereInTable())
+				{
+					// 명령어 칸으로 이동
+				}
+				else
+				{
+					// 1. 상호 부터 새로 입력
+				}
+			}
+		}
+
+		private bool IsThereInTable()
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
