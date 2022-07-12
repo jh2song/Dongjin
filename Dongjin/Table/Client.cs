f9a771a6181f8ea2fe0f745b9aa81ec0e2814b2c
Jihoon Song <spec0003@naver.com>
2022-07-12 15:30:52 +0900

    Feat: Add Ledger Table

--- Dongjin/Table/Client.cs
+++ Dongjin/Table/Client.cs
@@ -10,25 +10,25 @@ namespace Dongjin.Table
 		// 거래처코드
 		[PrimaryKey]
 		public int ClientCode { get; set; }
-		// 상호
+		// 1. 상호
 		public string ClientName { get; set; } = "";
-		// 전화번호
+		// 2. 전화번호
 		public string Phone { get; set; } = "";
-		// 현재미수금
+		// 3. 현재미수금
 		public int CurrentLeftMoney { get; set; } = 0;
-		// 단가(%)구분
+		// 4. 단가(%)구분
 		public int PercentCode { get; set; } = 0;
-		// 당일판매액
+		// 8. 당일판매액
 		public int TodaySellMoney { get; set; } = 0;
-		// 당일입금액
+		// 9. 당일입금액
 		public int TodayDepositMoney { get; set; } = 0;
-		// 당일환입액
+		// 10. 당일환입액
 		public int TodayReturnMoney { get; set; } = 0;
-		// 당월판매액
+		// 11. 당월판매액
 		public int MonthSellMoney { get; set; } = 0;
-		// 당월입금액
+		// 12. 당월입금액
 		public int MonthDepositMoney { get; set; } = 0;
-		// 당월환입액
+		// 13. 당월환입액
 		public int MonthReturnMoney { get; set; } = 0;
 	}
 }
---
+++ Dongjin/Table/LeftMoneyLedger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dongjin.Table
+{
+	class LeftMoneyLedger
+	{
+		// 처리일자
+		public DateTime NowDate { get; set; } = DateTime.MinValue;
+		// 거래처코드
+		public int ClientCode { get; set; }
+
+		// 거래일자
+		public DateTime TransactionDate { get; set; }
+		// 전일 미수
+		public int YesterdayLeftMoney { get; set; }
+		// 금일 판매
+		public int TodaySellMoney { get; set; }
+		// 금일 환입
+		public int TodayRefundMoney { get; set; }
+		// 금일 입금
+		public int TodayDepositMoney { get; set; }
+		// 현재 미수
+		public int CurrentLeftMoney { get; set; }
+		// 금액
+		public int Price { get; set; }
+	}
+}
