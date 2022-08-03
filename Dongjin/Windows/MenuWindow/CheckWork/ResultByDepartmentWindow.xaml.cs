2a8707f96638db7c4afdc2ca6e6e326361eff17e
Jihoon Song <spec0003@naver.com>
2022-08-03 09:51:30 +0900

    Feat Implement ResultByDepartmentWindow

--- Dongjin/Windows/MenuWindow/CheckWork/ResultByDepartmentWindow.xaml.cs
+++ Dongjin/Windows/MenuWindow/CheckWork/ResultByDepartmentWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace Dongjin.Windows.MenuWindow.CheckWork
 	public partial class ResultByDepartmentWindow : Window
 	{
 		private int _choice;
+		private DateTime _dateTime;
 
 		public ResultByDepartmentWindow()
 		{
@@ -68,17 +69,93 @@ namespace Dongjin.Windows.MenuWindow.CheckWork
 
 		private void YearTB_KeyDown(object sender, KeyEventArgs e)
 		{
+			if (e.Key == Key.Escape && YearTB.Text == "")
+			{
+				ChoiceTB.Text = "";
+				ChoiceTB.Focus();
+			}
 
+			if (e.Key == Key.Escape && YearTB.Text != "")
+			{
+				YearTB.Text = "";
+			}
+
+			if (e.Key == Key.Enter)
+			{
+				if (YearTB.Text.Length < 2)
+					YearTB.Text = DateTime.Now.Year.ToString().Substring(2, 2);
+
+				MonthTB.Focus();
+			}
 		}
 
 		private void MonthTB_KeyDown(object sender, KeyEventArgs e)
 		{
+			if (e.Key == Key.Escape)
+			{
+				if (MonthTB.Text == "")
+				{
+					YearTB.Text = "";
+					YearTB.Focus();
+				}
+				else
+				{
+					MonthTB.Text = "";
+				}
+			}
 
+			if (e.Key == Key.Enter)
+			{
+				if (MonthTB.Text.Length < 2)
+					MonthTB.Text = DateTime.Now.Month.ToString("00");
+
+				DayTB.Focus();
+			}
 		}
 
 		private void DayTB_KeyDown(object sender, KeyEventArgs e)
 		{
+			if (e.Key == Key.Escape)
+			{
+				if (DayTB.Text == "")
+				{
+					MonthTB.Text = "";
+					MonthTB.Focus();
+				}
+				else
+				{
+					DayTB.Text = "";
+				}
+			}
 
+			if (e.Key == Key.Enter)
+			{
+				if (DayTB.Text.Length < 2)
+					DayTB.Text = DateTime.Now.Day.ToString("00");
+
+				if (VerifyDate() == false)
+				{
+					YearTB.Text = MonthTB.Text = DayTB.Text = "";
+					YearTB.Focus();
+
+					return;
+				}
+			}
+		}
+
+		private bool VerifyDate()
+		{
+			try
+			{
+				_dateTime = new DateTime(int.Parse("20" + YearTB.Text), int.Parse(MonthTB.Text), int.Parse(DayTB.Text));
+				return true;
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("날짜가 잘못되었습니다.", "날짜 오류", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+
+			return false;
 		}
 	}
 }
