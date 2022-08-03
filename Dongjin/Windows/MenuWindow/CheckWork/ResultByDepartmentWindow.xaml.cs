d7f10faf308379984fdcb899ce12837aec465045
Jihoon Song <spec0003@naver.com>
2022-08-03 09:29:54 +0900

    Feat: Architecture ResultByDepartmentWindow

--- Dongjin/Windows/MenuWindow/CheckWork/ResultByDepartmentWindow.xaml.cs
+++ Dongjin/Windows/MenuWindow/CheckWork/ResultByDepartmentWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dongjin.Classes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,9 +18,67 @@ namespace Dongjin.Windows.MenuWindow.CheckWork
 	/// </summary>
 	public partial class ResultByDepartmentWindow : Window
 	{
+		private int _choice;
+
 		public ResultByDepartmentWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void ChoiceTB_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			e.Handled = RegexClass.NotNumericBackspace(e.Text);
+		}
+
+		private void YearTB_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			e.Handled = RegexClass.NotNumericBackspace(e.Text);
+		}
+
+		private void MonthTB_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			e.Handled = RegexClass.NotNumericBackspace(e.Text);
+		}
+
+		private void DayTB_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			e.Handled = RegexClass.NotNumericBackspace(e.Text);
+		}
+
+		private void ChoiceTB_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Escape && ChoiceTB.Text == "")
+			{
+				Close();
+			}
+
+			if (e.Key == Key.Escape && ChoiceTB.Text != "")
+			{
+				ChoiceTB.Text = "";
+			}
+
+			if (e.Key == Key.Enter && int.TryParse(ChoiceTB.Text, out _choice))
+			{
+				if (_choice == 1 || _choice == 2)
+				{
+					YearTB.Focus();
+				}
+			}
+		}
+
+		private void YearTB_KeyDown(object sender, KeyEventArgs e)
+		{
+
+		}
+
+		private void MonthTB_KeyDown(object sender, KeyEventArgs e)
+		{
+
+		}
+
+		private void DayTB_KeyDown(object sender, KeyEventArgs e)
+		{
+
 		}
 	}
 }
