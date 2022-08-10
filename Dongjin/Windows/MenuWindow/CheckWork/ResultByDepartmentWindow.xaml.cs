d4c903cb6a350398135355c39a57fca2c5841258
Jihoon Song <spec0003@naver.com>
2022-08-10 13:44:10 +0900

    Fix: Fix putting controls to list
    _todayLabelList, _monthLabelList

--- Dongjin/Windows/MenuWindow/CheckWork/ResultByDepartmentWindow.xaml.cs
+++ Dongjin/Windows/MenuWindow/CheckWork/ResultByDepartmentWindow.xaml.cs
@@ -22,7 +22,8 @@ namespace Dongjin.Windows.MenuWindow.CheckWork
 	{
 		private int _choice;
 		private DateTime _dateTime;
-		private List<List<Label>> _labelControlList;
+		private List<List<Label>> _todayLabelList;
+		private List<List<Label>> _monthLabelList;
 
 		public ResultByDepartmentWindow()
 		{
@@ -35,32 +36,123 @@ namespace Dongjin.Windows.MenuWindow.CheckWork
 
 		private void SetControlByList()
 		{
-			_labelControlList = new List<List<Label>>();
-			List<Label> labels = new List<Label>();
-			labels.Add(Today01SellMoneyLB);
-			labels.Add(Today01RefundMoneyLB);
-			labels.Add(Today01DepositMoneyLB);
-			labels.Add(Today01SellCountLB);
-			labels.Add(Today01RefundCountLB);
-			_labelControlList.Add(labels);
-			labels.Clear();
+			_todayLabelList = new List<List<Label>>();
 
-			labels.Add(Today02SellMoneyLB);
-			labels.Add(Today02RefundMoneyLB);
-			labels.Add(Today02DepositMoneyLB);
-			labels.Add(Today02SellCountLB);
-			labels.Add(Today02RefundCountLB);
-			_labelControlList.Add(labels);
-			labels.Clear();
+			_todayLabelList.Add(new List<Label>() { 
+				Today01SellMoneyLB, 
+				Today01RefundMoneyLB, 
+				Today01DepositMoneyLB, 
+				Today01SellCountLB, 
+				Today01RefundCountLB 
+			});
+			_todayLabelList.Add(new List<Label>() {
+				Today02SellMoneyLB,
+				Today02RefundMoneyLB,
+				Today02DepositMoneyLB,
+				Today02SellCountLB,
+				Today02RefundCountLB
+			});
+			_todayLabelList.Add(new List<Label>() {
+				Today03SellMoneyLB,
+				Today03RefundMoneyLB,
+				Today03DepositMoneyLB,
+				Today03SellCountLB,
+				Today03RefundCountLB
+			});
+			_todayLabelList.Add(new List<Label>() {
+				Today04SellMoneyLB,
+				Today04RefundMoneyLB,
+				Today04DepositMoneyLB,
+				Today04SellCountLB,
+				Today04RefundCountLB
+			});
+			_todayLabelList.Add(new List<Label>() {
+				Today05SellMoneyLB,
+				Today05RefundMoneyLB,
+				Today05DepositMoneyLB,
+				Today05SellCountLB,
+				Today05RefundCountLB
+			});
+			_todayLabelList.Add(new List<Label>() {
+				Today06SellMoneyLB,
+				Today06RefundMoneyLB,
+				Today06DepositMoneyLB,
+				Today06SellCountLB,
+				Today06RefundCountLB
+			});
+			_todayLabelList.Add(new List<Label>() {
+				Today07SellMoneyLB,
+				Today07RefundMoneyLB,
+				Today07DepositMoneyLB,
+				Today07SellCountLB,
+				Today07RefundCountLB
+			});
+			_todayLabelList.Add(new List<Label>() {
+				TodayTotalSellMoneyLB,
+				TodayTotalRefundMoneyLB,
+				TodayTotalDepositMoneyLB,
+				TodayTotalSellCountLB,
+				TodayTotalRefundCountLB
+			});
 
-			labels.Add(Today03SellMoneyLB);
-			labels.Add(Today03RefundMoneyLB);
-			labels.Add(Today03DepositMoneyLB);
-			labels.Add(Today03SellCountLB);
-			labels.Add(Today03RefundCountLB);
-			_labelControlList.Add(labels);
-			labels.Clear();
+			_monthLabelList = new List<List<Label>>();
 
+			_monthLabelList.Add(new List<Label>() {
+				Month01SellMoneyLB,
+				Month01RefundMoneyLB,
+				Month01DepositMoneyLB,
+				Month01SellCountLB,
+				Month01RefundCountLB
+			});
+			_monthLabelList.Add(new List<Label>() {
+				Month02SellMoneyLB,
+				Month02RefundMoneyLB,
+				Month02DepositMoneyLB,
+				Month02SellCountLB,
+				Month02RefundCountLB
+			});
+			_monthLabelList.Add(new List<Label>() {
+				Month03SellMoneyLB,
+				Month03RefundMoneyLB,
+				Month03DepositMoneyLB,
+				Month03SellCountLB,
+				Month03RefundCountLB
+			});
+			_monthLabelList.Add(new List<Label>() {
+				Month04SellMoneyLB,
+				Month04RefundMoneyLB,
+				Month04DepositMoneyLB,
+				Month04SellCountLB,
+				Month04RefundCountLB
+			});
+			_monthLabelList.Add(new List<Label>() {
+				Month05SellMoneyLB,
+				Month05RefundMoneyLB,
+				Month05DepositMoneyLB,
+				Month05SellCountLB,
+				Month05RefundCountLB
+			});
+			_monthLabelList.Add(new List<Label>() {
+				Month06SellMoneyLB,
+				Month06RefundMoneyLB,
+				Month06DepositMoneyLB,
+				Month06SellCountLB,
+				Month06RefundCountLB
+			});
+			_monthLabelList.Add(new List<Label>() {
+				Month07SellMoneyLB,
+				Month07RefundMoneyLB,
+				Month07DepositMoneyLB,
+				Month07SellCountLB,
+				Month07RefundCountLB
+			});
+			_monthLabelList.Add(new List<Label>() {
+				MonthTotalSellMoneyLB,
+				MonthTotalRefundMoneyLB,
+				MonthTotalDepositMoneyLB,
+				MonthTotalSellCountLB,
+				MonthTotalRefundCountLB
+			});
 		}
 
 		private void ChoiceTB_PreviewTextInput(object sender, TextCompositionEventArgs e)
@@ -280,6 +372,19 @@ WHERE a.ClientCode = b.ClientCode AND a.TransactionDate = ?
 
 		private void ShowGrid()
 		{
+			// 일단 모두 0으로 초기화
+			for (int i = 0; i < _todayLabelList.Count; i++)
+				for (int j = 0; j < _todayLabelList[i].Count; j++)
+					_todayLabelList[i][j].Content = "0";
+			for (int i = 0; i < _monthLabelList.Count; i++)
+				for (int j = 0; j < _monthLabelList[i].Count; j++)
+					_monthLabelList[i][j].Content = "0";
+
+			SetTodayContent();
+		}
+
+		private void SetTodayContent()
+		{
 			try
 			{
 				var todayMoney = DB.Conn.Query<Money>(@"
@@ -294,32 +399,52 @@ FROM Document
 WHERE TransactionDate = ?
 GROUP BY SUBSTR(ClientCode, 1, 1);
 ", _dateTime);
-				
+
+				int sumSellMoney = 0;
+				int sumRefundMoney = 0;
+				int sumDepositMoney = 0;
+				int sumSellCount = 0;
+				int sumRefundCount = 0;
+
 				foreach (Money money in todayMoney)
 				{
-					switch (money.Department)
-					{
-						case 1:
-							break;
-						case 2:
-							break;
-						case 3:
-							break;
-						case 4:
-							break;
-						case 5:
-							break;
-						case 6:
-							break;
-						case 7:
-							break;
-					}
+					if (money.Department < 1 || money.Department > 7)
+						continue;
+
+					// 각 과의 판매 금액
+					_todayLabelList[money.Department - 1][0].Content = String.Format("{0:#,0}", money.SellMoney);
+					// 각 과의 환입 금액
+					_todayLabelList[money.Department - 1][1].Content = String.Format("{0:#,0}", money.RefundMoney);
+					// 각 과의 입금 금액
+					_todayLabelList[money.Department - 1][2].Content = String.Format("{0:#,0}", money.DepositMoney);
+
+					sumSellMoney += money.SellMoney;
+					sumRefundMoney += money.RefundMoney;
+					sumDepositMoney += money.DepositMoney;
+				}
+				foreach (Count count in todayCount)
+				{
+					if (count.Department < 1 || count.Department > 7)
+						continue;
+
+					// 각 과의 판매 수
+					_todayLabelList[count.Department - 1][3].Content = String.Format("{0:#,0}", count.SellCount);
+					// 각 과의 환입 수
+					_todayLabelList[count.Department - 1][4].Content = String.Format("{0:#,0}", count.RefundCount);
+
+					sumSellCount += count.SellCount;
+					sumRefundCount += count.RefundCount;
 				}
 
+				_todayLabelList.Last()[0].Content = String.Format("{0:#,0}", sumSellMoney);
+				_todayLabelList.Last()[1].Content = String.Format("{0:#,0}", sumRefundMoney);
+				_todayLabelList.Last()[2].Content = String.Format("{0:#,0}", sumDepositMoney);
+				_todayLabelList.Last()[3].Content = String.Format("{0:#,0}", sumSellCount);
+				_todayLabelList.Last()[4].Content = String.Format("{0:#,0}", sumRefundCount);
 			}
 			catch (Exception)
 			{
-				MessageBox.Show("과별집계 테이블을 불러오는데 실패하였습니다.", "DB 오류", MessageBoxButton.OK, MessageBoxImage.Error);
+				MessageBox.Show("당일분을 불러오는데 실패하였습니다.", "DB 오류", MessageBoxButton.OK, MessageBoxImage.Error);
 			}
 		}
 	}
