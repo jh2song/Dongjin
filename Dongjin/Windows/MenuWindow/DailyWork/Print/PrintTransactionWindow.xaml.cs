ada935389566e0e821e05ae1757efa3ef7696acd
Jihoon Song <spec0003@naver.com>
2022-10-17 12:59:01 +0900

    Feat: Add date, clientinfo in invoice

--- Dongjin/Windows/MenuWindow/DailyWork/Print/PrintTransactionWindow.xaml.cs
+++ Dongjin/Windows/MenuWindow/DailyWork/Print/PrintTransactionWindow.xaml.cs
@@ -18,13 +18,39 @@ namespace Dongjin.Windows.MenuWindow.DailyWork.Print
     /// </summary>
     public partial class PrintTransactionWindow : Window
     {
+        string _printOption;
+        DateTime _transactionDate;
+        Client _foundClient;
+        DataGrid _DG;
+
         public PrintTransactionWindow(string printOption, DateTime transactionDate, Client foundClient, DataGrid DG)
         {
             InitializeComponent();
 
+            _printOption = printOption;
+            _transactionDate = transactionDate;
+            _foundClient = foundClient;
+            _DG = DG;
 
+            SetDate();
+            SetClientInfo();
         }
 
+		private void SetDate()
+		{
+            LBDate.Content = $"{_transactionDate.Year.ToString("0000")}년 {_transactionDate.Month.ToString("00")}월 {_transactionDate.Day.ToString("00")}일";
+        }
 
+        private void SetClientInfo()
+        {
+            string outClientInfo = "";
+            outClientInfo += "거래처코드: ";
+            outClientInfo += _foundClient.ClientCode.ToString() + "   ";
+            outClientInfo += "전화: ";
+            outClientInfo += _foundClient.Phone.ToString() + "   ";
+            outClientInfo += "거래처명: ";
+            outClientInfo +=  _foundClient.ClientName.ToString();
+            LBClientInfo.Content = outClientInfo;
+        }
     }
 }
