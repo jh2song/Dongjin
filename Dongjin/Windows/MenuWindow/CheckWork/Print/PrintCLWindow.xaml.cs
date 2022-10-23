c1f395666a8596401993faf710a7d6f38c4e3e79
Jihoon Song <spec0003@naver.com>
2022-10-23 12:53:50 +0900

    temp backup

---
+++ Dongjin/Windows/MenuWindow/CheckWork/Print/PrintCLWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dongjin.Windows.MenuWindow.CheckWork.Print
+{
+    /// <summary>
+    /// Interaction logic for PrintCLWindow.xaml
+    /// </summary>
+    public partial class PrintCLWindow : Window
+    {
+        public PrintCLWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
